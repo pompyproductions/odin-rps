4542c23d6d4e8663599e76438f50c262e34c7653
Eren Gazioglu <pompyproduction@gmail.com>
2022-06-05 00:57:17 +0100

    Create computerMove function

--- rps.js
+++ rps.js
@@ -1,1 +1,21 @@
 console.log("Floatation device");
+
+const MOVES = ["rock", "paper", "scissors"];
+
+// Math.random() gives a value between 0 and 1
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function computerMove() {
+    let newMove = MOVES[getRandomInt(0,2)];
+    console.log(newMove);
+}
+
+computerMove();
+computerMove();
+computerMove();
+computerMove();
+computerMove();
+computerMove();
+computerMove();
